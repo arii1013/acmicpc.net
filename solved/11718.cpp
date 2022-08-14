487c655f87051933df095b3648b4091070ddfe2e
Ariel Omar Lee <sonrisauba@gmail.com>
2022-08-15 00:55:16 +0900

    Nº11718 solved

---
+++ solved/11718.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define ll long long
+
+int main() {
+    // FAST_IO
+
+    // 입력
+    while (true) {
+        string s; getline(cin, s);
+        if (s == "") break;
+        cout << s << "\n";
+    }
+    return 0;
+}
